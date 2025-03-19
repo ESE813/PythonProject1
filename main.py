ad6f480a8a0179182ea686ca8c8f6828edf84bc1
Elena <elenaolgaegorova@yandex.ru>
2025-03-19 13:06:29 +0300

    add func gate_date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_card_number, get_mask_account
+from src.widget import get_date
 
 
 if __name__ == "__main__":
@@ -6,3 +7,5 @@ if __name__ == "__main__":
     account_number = "0000000000000000000"
     print(get_mask_card_number(card_number))
     print(get_mask_account(account_number))
+    date = "2024-03-11T02:26:18.671407"
+    print(get_date(date))
---
+++ src/widget.py
@@ -0,0 +1,30 @@
+from calendar import month
+from typing import Union
+
+
+def mask_account_card(card_number: Union[str]) -> Union[str]:
+    """Функция принимает тип и номер карты или счета и маскирует его"""
+
+    if card_number.isdigit() and len(card_number) == 16:
+        masked_number = card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
+        return masked_number
+    else:
+        return "Неправильно введен номер карты!"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счета в виде числа и
+    возвращает маску номера счета."""
+    if account_number.isdigit() and len(account_number) == 20:
+        masked_account = "**" + account_number[-4:]
+        return masked_account
+    else:
+        return "Неправильно введен номер счета!"
+
+
+def get_date(date_str: str) -> str:
+    """Функция обрабатывает дату в формате ДД-ММ-ГГ"""
+    year_str = date_str[:4]
+    month_str = date_str[5:7]
+    day_str = date_str[8:10]
+    return f'{day_str}.{month_str}.{year_str}'
