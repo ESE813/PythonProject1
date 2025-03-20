e552fbdc1076ab98d1a7dd58259b6ca347a6171b
Elena <elenaolgaegorova@yandex.ru>
2025-03-20 13:16:41 +0300

    homework corrected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.masks import get_mask_card_number, get_mask_account
-from src.widget import get_date
+from src.widget import get_date, mask_account_card
 
 
 if __name__ == "__main__":
@@ -9,3 +9,9 @@ if __name__ == "__main__":
     print(get_mask_account(account_number))
     date = "2024-03-11T02:26:18.671407"
     print(get_date(date))
+    print(mask_account_card('Maestro 1596837868705199'))
+    print(mask_account_card('Счет 64686473678894779589'))
+    print(mask_account_card('MasterCard 7158300734726758'))
+    print(mask_account_card('Счет 35383033474447895560'))
+    print(mask_account_card('Visa Classic 6831982476737658'))
+    print(mask_account_card('Visa Platinum 8990922113665229'))
--- src/widget.py
+++ src/widget.py
@@ -1,25 +1,21 @@
-from calendar import month
-from typing import Union
+from src.masks import get_mask_card_number, get_mask_account
 
-
-def mask_account_card(card_number: Union[str]) -> Union[str]:
-    """Функция принимает тип и номер карты или счета и маскирует его"""
-
-    if card_number.isdigit() and len(card_number) == 16:
-        masked_number = card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-        return masked_number
+def mask_account_card(num_for_mask: str) -> str:
+    """Функция маскирует номер карты или счета """
+    num_for_mask_split = num_for_mask.split()
+    if "Cчет" in num_for_mask:
+        return f"Cчет {get_mask_account} {num_for_mask_split}"
     else:
-        return "Неправильно введен номер карты!"
-
-
-def get_mask_account(account_number: str) -> str:
-    """Функция принимает на вход номер счета в виде числа и
-    возвращает маску номера счета."""
-    if account_number.isdigit() and len(account_number) == 20:
-        masked_account = "**" + account_number[-4:]
-        return masked_account
-    else:
-        return "Неправильно введен номер счета!"
+        card_num = []
+        card_name = []
+        for i in num_for_mask_split:
+            if i.isdigit():
+                card_num.append(i)
+            if i.isalpha():
+                card_name.append(i)
+        str_card_num = ' '.join(card_num)
+        str_card_name = ' '.join(card_name)
+        return f"{str_card_name} {get_mask_card_number(str_card_num)}"
 
 
 def get_date(date_str: str) -> str:
@@ -28,3 +24,6 @@ def get_date(date_str: str) -> str:
     month_str = date_str[5:7]
     day_str = date_str[8:10]
     return f"{day_str}.{month_str}.{year_str}"
+
+
+
