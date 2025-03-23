cbad49fb2c07aa17d828bfeade849d8269eb8309
Elena <elenaolgaegorova@yandex.ru>
2025-03-23 08:35:25 +0300

    add func filter_by_state

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.masks import get_mask_account
 from src.masks import get_mask_card_number
 from src.widget import get_date
 from src.widget import mask_account_card
+from src.processing import filter_by_state
 
 if __name__ == "__main__":
     card_number = "0000000000000000"
@@ -16,3 +17,13 @@ if __name__ == "__main__":
     print(mask_account_card("Счет 35383033474447895560"))
     print(mask_account_card("Visa Classic 6831982476737658"))
     print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(
+        filter_by_state(
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ]
+        )
+    )
--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,3 @@
-def filter_by_state()
+def filter_by_state(bank_data, state="EXECUTED"):
+    """Фильтрует словарь по значению ключа state"""
+    return [item for item in bank_data if item.get("state") == state]
